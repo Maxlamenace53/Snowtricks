44364fbcff89fdbc04f8f68b3fb7293ae26333ba
Maxime Lemée <maxime.lemee1@gmail.com>
2023-04-30 15:14:41 +0200

    Affichage responsive ok pour le show trick et acceuil

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Trick;
 use App\Repository\GroupTrickRepository;
+use App\Repository\PhotoTrickRepository;
 use App\Repository\TrickRepository;
+use App\Repository\VideoTrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,14 +14,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(TrickRepository $trickRepository, GroupTrickRepository $groupTrickRepository): Response
+    public function index(TrickRepository $trickRepository, GroupTrickRepository $groupTrickRepository, PhotoTrickRepository $photoTrickRepository, VideoTrickRepository $videoTrickRepository): Response
     {
         $lastTrick = $trickRepository->findOneBy([], ['creationDate' => 'DESC']);
+        $lastPhoto = $photoTrickRepository->findOneBy([], ['dateAdded' => 'DESC']);
+        $lastVideo = $videoTrickRepository->findOneBy([], ['dateAdded' => 'DESC']);
         $groups = $groupTrickRepository->findAll();
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'LastTrick' => $lastTrick,
-            'groups'=> $groups
+            'LastPhoto' => $lastPhoto,
+            'LastVideo'=> $lastVideo,
+            'groups' => $groups
 
         ]);
     }
