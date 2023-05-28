16329e404d22b02b3e33498f559120ca71e74da0
Maxime Lemée <maxime.lemee1@gmail.com>
2023-05-28 14:56:57 +0200

    Amélioration visuelle divers + traduction

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,11 +3,14 @@
 namespace App\Form;
 
 use App\Entity\User;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -21,14 +24,42 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             //->add('avatar')
-            ->add('firstName',)
-            ->add('email', EmailType::class)
-            ->add('lastName')
-            ->add('nickname')
-            ->add('description', TextareaType::class)
-            ->add('registrationDate')
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => 'content'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'content'
+                ]
+
+            ])
+            ->add('lastName', TextType::class, [
+                    'label' => 'Nom',
+                    'attr' => [
+                        'class' => 'content'
+                    ]
+                ]
+            )
+            ->add('nickname', TextType::class, [
+                'label' => 'Pseudo',
+                'attr' => [
+                    'class' => 'content'
+                ]
+            ])
+            ->add('avatar', FileType::class)
+            ->add('description', CKEditorType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'class' => 'content'
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => 'Accepter les conditions',
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -39,10 +70,14 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'nouveau-mot-de-passe'],
+                'label' => 'Mot de passe',
+                'attr'=>[
+                    'class'=>'content'
+                ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez rentrez votre mot de passe'
                     ]),
                     new Length([
                         'min' => 6,
@@ -51,8 +86,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
