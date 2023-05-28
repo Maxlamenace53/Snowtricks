d023cbd7f1d81cc4e2b1c17f91b467a60cd417e0
Maxime Lemée <maxime.lemee1@gmail.com>
2023-05-28 14:55:35 +0200

    Carrousel Swiper

--- assets/app.js
+++ assets/app.js
@@ -14,7 +14,36 @@ import './styles/style.scss';
 // start the Stimulus application
 import './bootstrap';
 
-//registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
+// import Swiper JS
+import Swiper from 'swiper/bundle';
+// import Swiper styles
+import 'swiper/css/bundle';
+//carousel gallery trick
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    on: {
+        autoplayTimeLeft(s, time, progress) {
+            progressCircle.style.setProperty("--progress", 1 - progress);
+            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+    }
+});
 
 
 function readURL(input) { //nom de la function à appeler dans l'attribut onchange de l'input
